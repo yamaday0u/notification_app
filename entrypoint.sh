0000b793ca990bb80cc63099f9253aa7687a7360
yamaday0u <35hallelujah00@gmail.com>
2021-09-29 22:49:32 +0900

    entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# エラーが発生するとスクリプトを終了する
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+# Railsのpidが存在している場合に削除する/Rails特有の問題への対処
+rm -f /notification_app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+# DockerfileのCMDで渡されたコマンド（→Railsのサーバー起動）を実行
+exec "$@"
